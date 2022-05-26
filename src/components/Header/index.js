1cf8471f4f36d2f462bc39dde9a21a395039e009
jully-nixon <jully.niixon@gmail.com>
2022-05-26 11:02:13 -0300

    Estrutura do header criada.

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import Logo from '../../assets/images/logo.png';
+import './style.css';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar>
+                <NavLink to="/"><img alt="logo" src={Logo} /></NavLink>
+                <div className='header--container'>
+                    <NavLink className="header--itens" to="/">Sobre</NavLink>
+                    <NavLink className="header--itens" to="/">Ações</NavLink>
+                    <NavLink className="header--itens" to="/">Lojinha</NavLink>
+                    <NavLink className="header--itens" to="/">Nosso time</NavLink>
+                    <NavLink className="header--itens" to="/">Parcerias</NavLink>
+                    <NavLink className="header--itens" to='/contact'>Contato</NavLink>
+                </div>
+            </Navbar>
+        </>
+    );
+}
+
+export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Home from '../screens/Home';
 import { Routes, Route } from 'react-router-dom';
+import Contact from '../screens/Contact';
+import Header from '../components/Header';
 
 const Rotas = () => {
     return (
         <div>
+            <Header />
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route exact path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </div>
     )
