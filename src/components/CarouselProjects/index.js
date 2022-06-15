ae78b0df916744406c8f35b854afc81e70b9e62e
jully-nixon <jully.niixon@gmail.com>
2022-06-15 15:18:58 -0300

    Fontes aplicadas aos textos.

--- src/components/CarouselProjects/index.js
+++ src/components/CarouselProjects/index.js
@@ -1,70 +1,23 @@
 import React from 'react';
 import './style.css';
-import BannerImage from '../../assets/images/banner.jpg';
 import BackgroundProjectBorder from '../../assets/images/bg_project_border.svg';
-// import ArrowLeft from '../../assets/images/proj_arrow_left.svg';
-// import ArrowRight from '../../assets/images/proj_arrow_right.svg';
 import Carousel from 'react-elastic-carousel';
 
-const CarouselProjects = ({ handlerButton }) => {
+const CarouselProjects = ({ handlerButton, image, title }) => {
 
     return (
         <div>
             <Carousel
                 itemsToShow={3}
                 pagination={false}
-                // enableAutoPlay={true}
-                // autoPlaySpeed={5000}
             >
                 <div className='carousel-projects-background' onClick={handlerButton}>
                     <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
+                    <img className='carousel-projects-image' src={image} alt='projects' />
                     <label className='carousel-projects-title'>
-                        Technovation Girls
+                        {title}
                     </label>
                 </div>
-                <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div>
-                <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div>
-                <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div>
-                {/* <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div>
-                <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div>
-                <div className='carousel-projects-background'>
-                    <img className='carousel-projects-border' src={BackgroundProjectBorder} alt='BackgroundProjectBorder' />
-                    <img className='carousel-projects-image' src={BannerImage} alt='projects' />
-                    <label className='carousel-projects-title'>
-                        Technovation Girls
-                    </label>
-                </div> */}
             </Carousel>;
         </div>
     )
