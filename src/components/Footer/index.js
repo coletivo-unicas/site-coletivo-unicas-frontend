bc0fda1c0e96d34d5378a71e97cfaf935741df8d
Marilia Vilar <marilia@roadmaps.com.br>
2022-06-20 01:46:05 -0300

    update: layout e textos do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,31 +6,50 @@ import InstagramSvg from '../../assets/icons/socialmedia/InstagramSvg';
 import './style.css';
 
 const Footer = () => {
-    return (
-        <footer className='footer-bg'>
-            <div className='footer-container'>
-                <div className='footer-social-media'>
-                    <a href='mailto:coletivounicas@gmail.com' target="_blank" rel="noreferrer">
-                        <EmailSvg className='icon-social-media' />
-                    </a>
-                    <a href='https://www.linkedin.com/company/coletivo-unicas' target="_blank" rel="noreferrer">
-                        <LinkedinSvg className='icon-social-media' />
-                    </a>
-                    <a href='https://github.com/coletivo-unicas' target="_blank" rel="noreferrer">
-                        <GithubSvg className='icon-social-media' />
-                    </a>
-                    <a href='https://www.instagram.com/coletivo.unicas' target="_blank" rel="noreferrer">
-                        <InstagramSvg className='icon-social-media' />
-                    </a>
+  return (
+    <footer className="footer-bg">
+      <div className="footer-container">
+        <div className="footer-social-media">
+          <a
+            id="footer-button"
+            href="mailto:coletivounicas@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <EmailSvg className="icon-social-media" />
+          </a>
+          <a
+            id="footer-button"
+            href="https://www.linkedin.com/company/coletivo-unicas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedinSvg className="icon-social-media" />
+          </a>
+          <a
+            id="footer-button"
+            href="https://github.com/coletivo-unicas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GithubSvg className="icon-social-media" />
+          </a>
+          <a
+            id="footer-button"
+            href="https://www.instagram.com/coletivo.unicas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <InstagramSvg className="icon-social-media" />
+          </a>
+        </div>
 
-                </div>
-
-                <p>Desenvolvido por Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-                <p>Coletivo Unicas - Direitos reservados</p>
-
-            </div>
-        </footer>
-    )
+        <h6>Desenvolvido por Caroline, Jad, Jully, Mayara e Marília</h6>
+        <h6>Mantido pelo Coletivo Unicas</h6>
+        <h6>Unicap - Recife/PE - Brasil</h6>
+      </div>
+    </footer>
+  );
 };
 
-export default Footer
+export default Footer;
