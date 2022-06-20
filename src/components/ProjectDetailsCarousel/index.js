5f509eeedd8ef9d10e16021f9b15f76ec236aceb
jully-nixon <jully.niixon@gmail.com>
2022-06-19 22:51:34 -0300

    Lazy Loading adicionado as imagens em projetos.

--- src/components/ProjectDetailsCarousel/index.js
+++ src/components/ProjectDetailsCarousel/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './style.css';
 import Carousel from 'react-elastic-carousel';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const ProjectDetailsCarousel = ({ data }) => {
     return (
@@ -13,7 +15,13 @@ const ProjectDetailsCarousel = ({ data }) => {
                 autoPlaySpeed={2000}
             >
                 {data.map((url, id) =>
-                    <img key={id} className='project-details-carousel-image' src={url.image} alt='projects' />
+                    <LazyLoadImage
+                        key={id}
+                        className='project-details-carousel-image'
+                        src={url.image}
+                        alt='projects'
+                        effect="blur"
+                    />
                 )}
             </Carousel>
         </div>
